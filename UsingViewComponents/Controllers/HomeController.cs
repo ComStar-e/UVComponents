f2c115575a9f42b249e65f7a5ffab95bfe294254
Vitaliy Brichlov <brichkovv@gmail.com>
2019-08-06 19:00:40 +0300

    Add project files.

---
+++ UsingViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Controllers
+{
+    public class HomeController :Controller
+    {
+        private IProductRepository repository;
+        public HomeController(IProductRepository repo) => repository = repo;
+
+
+
+    }
+}
---
+++ UsingViewComponents/Models/City.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace UsingViewComponents.Models
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Population { get; set; } 
+    }
+}
